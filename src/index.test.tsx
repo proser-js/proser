b37b5eca8579c5fa89a6cb06728ebefc3ba74f83
Jared Lunde <jared.lunde@gmail.com>
2020-08-16 12:27:35 -0600

    feat: add react hooks

--- src/index.test.tsx
+++ src/index.test.tsx
@@ -1,9 +1,259 @@
 // import React from 'react'
-// import {renderHook} from '@testing-library/react-hooks'
-// import {render} from '@testing-library/react'
-// import userEvent from '@testing-library/user-event
-describe('proser', () => {
+import {renderHook, act} from '@testing-library/react-hooks'
+import {
+  usePaginate,
+  useOrder,
+  useTag,
+  useCategory,
+  useRelatedPosts,
+} from './index'
+
+const defaultPosts = [
+  {
+    id: 0,
+    metadata: {
+      title: 'Hello cats',
+      tags: ['cat', 'mouse'],
+      categories: ['animals'],
+      popularity: 0.1,
+    },
+  },
+  {
+    id: 1,
+    metadata: {
+      title: 'Hello dogs',
+      tags: ['dog', 'mouse'],
+      categories: ['animals'],
+      popularity: 0.5,
+    },
+  },
+  {
+    id: 2,
+    metadata: {
+      title: 'Hello food',
+      tags: ['oatmeal', 'sushi'],
+      categories: ['food'],
+      popularity: 0.2,
+    },
+  },
+]
+
+describe('usePaginate()', () => {
+  it('should incr on next', () => {
+    const {result} = renderHook(() =>
+      usePaginate(defaultPosts, {postsPerPage: 1})
+    )
+
+    expect(result.current[0]).toEqual([defaultPosts[0]])
+    expect(result.current[1].page).toBe(1)
+    expect(result.current[1].pages).toBe(3)
+
+    act(() => result.current[1].next())
+    expect(result.current[0]).toEqual([defaultPosts[1]])
+    expect(result.current[1].page).toBe(2)
+  })
+
+  it('should decr on prev', () => {
+    const {result} = renderHook(() =>
+      usePaginate(defaultPosts, {postsPerPage: 1, initialPage: 3})
+    )
+
+    expect(result.current[0]).toEqual([defaultPosts[2]])
+    expect(result.current[1].page).toBe(3)
+    expect(result.current[1].pages).toBe(3)
+
+    act(() => result.current[1].prev())
+    expect(result.current[0]).toEqual([defaultPosts[1]])
+    expect(result.current[1].page).toBe(2)
+  })
+
+  it('should set page', () => {
+    const {result} = renderHook(() =>
+      usePaginate(defaultPosts, {postsPerPage: 1})
+    )
+
+    expect(result.current[0]).toEqual([defaultPosts[0]])
+    expect(result.current[1].page).toBe(1)
+    expect(result.current[1].pages).toBe(3)
+
+    act(() => result.current[1].set(3))
+    expect(result.current[0]).toEqual([defaultPosts[2]])
+    expect(result.current[1].page).toBe(3)
+  })
+
+  it('should have correct page count', () => {
+    const {result} = renderHook(() => usePaginate(defaultPosts))
+
+    expect(result.current[0]).toEqual(defaultPosts)
+    expect(result.current[1].page).toBe(1)
+    expect(result.current[1].pages).toBe(1)
+  })
+
+  it('should bound itself', () => {
+    const {result} = renderHook(() => usePaginate(defaultPosts))
+
+    expect(result.current[0]).toEqual(defaultPosts)
+    expect(result.current[1].page).toBe(1)
+
+    act(() => result.current[1].next())
+    expect(result.current[1].page).toBe(1)
+
+    act(() => result.current[1].prev())
+    expect(result.current[1].page).toBe(1)
+
+    act(() => result.current[1].set(4))
+    expect(result.current[1].page).toBe(1)
+  })
+
+  it('should be controlled', () => {
+    let page = 1
+
+    const handleChange = jest.fn((nextPage: number) => {
+      page = nextPage
+    })
+
+    const {result, rerender} = renderHook(
+      ({page}: {page: number}) =>
+        usePaginate(defaultPosts, {
+          page,
+          postsPerPage: 1,
+          onChange: handleChange,
+        }),
+      {initialProps: {page}}
+    )
+
+    act(() => result.current[1].next())
+    expect(handleChange).toBeCalledWith(2, 1)
+
+    rerender({page})
+    expect(result.current[1].page).toBe(2)
+  })
+})
+
+describe('useOrder()', () => {
+  it('should default to desc', () => {
+    const {result} = renderHook(() => useOrder(defaultPosts))
+    expect(result.current).toEqual(defaultPosts.reverse())
+  })
+
+  it('should order by asc', () => {
+    const {result} = renderHook(() => useOrder(defaultPosts, 'asc'))
+    expect(result.current).toEqual(defaultPosts)
+  })
+
+  it('should order by popularity desc', () => {
+    const {result} = renderHook(() =>
+      useOrder(defaultPosts, {popularity: 'desc'})
+    )
+
+    expect(result.current).toEqual([
+      defaultPosts[1],
+      defaultPosts[2],
+      defaultPosts[0],
+    ])
+  })
+
+  it('should order by title asc', () => {
+    const {result} = renderHook(() => useOrder(defaultPosts, {title: 'asc'}))
+    expect(result.current).toEqual(defaultPosts)
+  })
+})
+
+describe('useTag()', () => {
   it('should pass', () => {
-    expect(true).toBe(true)
+    const {result} = renderHook(() => useTag(defaultPosts, 'cat'))
+    expect(result.current).toEqual([defaultPosts[0]])
+  })
+
+  it('should also pass', () => {
+    const {result} = renderHook(() => useTag(defaultPosts, 'mouse'))
+    expect(result.current).toEqual([defaultPosts[0], defaultPosts[1]])
+  })
+
+  it('should return empty if not found', () => {
+    const {result} = renderHook(() => useTag(defaultPosts, 'quetzalcoatl'))
+    expect(result.current).toEqual([])
+  })
+})
+
+describe('useCategory()', () => {
+  it('should pass', () => {
+    const {result} = renderHook(() => useCategory(defaultPosts, 'animals'))
+    expect(result.current).toEqual([defaultPosts[0], defaultPosts[1]])
+  })
+
+  it('should also pass', () => {
+    const {result} = renderHook(() => useCategory(defaultPosts, 'food'))
+    expect(result.current).toEqual([defaultPosts[2]])
+  })
+
+  it('should return empty if not found', () => {
+    const {result} = renderHook(() => useCategory(defaultPosts, 'quetzalcoatl'))
+    expect(result.current).toEqual([])
+  })
+})
+
+describe('useRelatedPosts()', () => {
+  it('should pass', () => {
+    const {result} = renderHook(() =>
+      useRelatedPosts(defaultPosts[0], defaultPosts)
+    )
+
+    expect(result.current).toEqual([defaultPosts[1], defaultPosts[2]])
+  })
+
+  it('should let tag outweigh category', () => {
+    const posts = [
+      {
+        id: 0,
+        metadata: {
+          title: 'Hello cats',
+          tags: ['cat', 'mouse'],
+          categories: ['animals'],
+          popularity: 0.1,
+        },
+      },
+      {
+        id: 1,
+        metadata: {
+          title: 'Hello dogs',
+          tags: ['dog', 'bird'],
+          categories: ['animals'],
+          popularity: 0.5,
+        },
+      },
+      {
+        id: 2,
+        metadata: {
+          title: 'Hello food',
+          tags: ['oatmeal', 'cat'],
+          categories: ['food'],
+          popularity: 0.2,
+        },
+      },
+    ]
+
+    const {result, rerender} = renderHook(
+      ({weight}) => useRelatedPosts(posts[0], posts, weight),
+      {
+        initialProps: {
+          weight: {
+            tags: 0.5,
+            categories: 1,
+          },
+        },
+      }
+    )
+
+    expect(result.current).toEqual([posts[1], posts[2]])
+
+    rerender({
+      weight: {
+        tags: 1,
+        categories: 0.5,
+      },
+    })
+
+    expect(result.current).toEqual([posts[2], posts[1]])
   })
 })
--- src/index.tsx
+++ src/index.tsx
@@ -1,3 +1,154 @@
-// import React from 'react'
-const Component = (): string => 'Hello world'
-export default Component
+import React from 'react'
+import useChange from '@react-hook/change'
+import {orderBy} from 'natural-orderby'
+
+export function usePaginate<T>(posts: T[], options: UsePaginateOptions = {}) {
+  const [page, setPage] = React.useState(
+    options.page ?? options.initialPage ?? 1
+  )
+  useChange(page, options.onChange || noop)
+  const currentPage = options.page ?? page
+  const postsPerPage = options.postsPerPage ?? 20
+  const pages = Math.ceil(posts.length / postsPerPage)
+  const start = postsPerPage * (currentPage - 1)
+  const bound = (value: number) => Math.min(Math.max(1, value), pages)
+
+  return [
+    posts.slice(start, start + postsPerPage),
+    {
+      page: currentPage,
+      pages,
+      set: (nextPage: number) => setPage(bound(nextPage)),
+      next: () => setPage((current) => bound(++current)),
+      prev: () => setPage((current) => bound(--current)),
+    },
+  ] as const
+}
+
+export interface UsePaginateOptions {
+  page?: number
+  /**
+   * @default 1
+   */
+  initialPage?: number
+  postsPerPage?: number
+  onChange?: (page: number, prevPage: number) => any
+}
+
+export function useOrder<T extends PostLike>(
+  posts: T[],
+  sortFn?:
+    | 'asc'
+    | 'desc'
+    | {[key in Extract<keyof T['metadata'], string>]?: 'asc' | 'desc'}
+    | ((a: T, b: T) => number)
+) {
+  sortFn = sortFn ?? 'desc'
+
+  return React.useMemo(() => {
+    if (typeof sortFn === 'object') {
+      return orderBy(
+        posts,
+        Object.keys(sortFn).map((key) => (v) => v.metadata[key]),
+        Object.values(sortFn) as ('asc' | 'desc')[]
+      )
+    }
+
+    return posts.sort(
+      sortFn === 'asc'
+        ? (a: any, b: any) => a.id - b.id
+        : sortFn === 'desc'
+        ? (a: any, b: any) => b.id - a.id
+        : sortFn
+    )
+  }, [posts, sortFn])
+}
+
+export function useTaxonomy<T extends PostLike>(
+  posts: T[],
+  type: string,
+  key: string
+) {
+  return React.useMemo(() => taxonomy(posts, type)[key] || [], [
+    posts,
+    type,
+    key,
+  ])
+}
+
+export function useTag<T extends PostLike>(posts: T[], tag: string) {
+  return useTaxonomy(posts, 'tags', tag)
+}
+
+export function useCategory<T extends PostLike>(posts: T[], category: string) {
+  return useTaxonomy(posts, 'categories', category)
+}
+
+/**
+ *
+ * @param posts
+ * @param weight
+ *
+ * @example
+ * useRelatedPosts(post, posts, {tags: 1, categories: 2})
+ */
+export function useRelatedPosts<T extends PostLike>(
+  post: T,
+  posts: T[],
+  weight: {[taxonomy: string]: number} = {tags: 0.5, categories: 1}
+) {
+  const weightKey = JSON.stringify(weight)
+
+  return React.useMemo(() => {
+    const weightKeys = Object.keys(weight)
+    const taxonomies = weightKeys.reduce((acc, key) => {
+      acc[key] = taxonomy(posts, key)
+      return acc
+    }, {} as Record<string, Record<string, T[]>>)
+    const weights = new Map<T, number>()
+
+    for (const taxonomy in taxonomies) {
+      const taxonomyMap = taxonomies[taxonomy]
+      const postTaxonomy = post.metadata[taxonomy]
+      // Skip if this taxonomy doesn't exist on the OP
+      if (postTaxonomy === void 0) continue
+      const matchable = Array.isArray(postTaxonomy)
+        ? postTaxonomy
+        : [postTaxonomy]
+
+      for (const value of matchable) {
+        const taxPosts = taxonomyMap[value]
+
+        for (let i = 0; i < taxPosts.length; i++) {
+          const taxPost = taxPosts[i]
+          if (post === taxPost) continue
+          const currentWeight = weights.get(taxPost) || 0
+          weights.set(taxPost, currentWeight + weight[taxonomy])
+        }
+      }
+    }
+
+    return posts
+      .filter((p) => p !== post)
+      .sort((a, b) => (weights.get(b) || 0) - (weights.get(a) || 0))
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [posts, weightKey])
+}
+
+export function taxonomy<T extends PostLike>(posts: T[], type: string) {
+  return Object.values(posts).reduce((acc, post) => {
+    for (const item of post.metadata[type] || []) {
+      acc[item] = acc[item] || []
+      acc[item].push(post)
+    }
+
+    return acc
+  }, {} as Record<string, T[]>)
+}
+
+function noop() {}
+
+export interface PostLike {
+  id: number
+  metadata: Record<string, any>
+}
